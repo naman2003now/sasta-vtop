78f4a1e2bd0ae6f33169a92bc3b2c1717ea772ea
naman2003now <a6.namanagrawal@gmail.com>
2022-05-29 17:50:57 +0530

    App class

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const { express } = require("express");
+const cors = require("cors");
+
+class App {
+  app = express();
+
+  constructor(controllers) {
+    this.#useMiddlewares();
+    this.#initHomeRoute();
+    this.#listen();
+  }
+
+  #useMiddlewares() {
+    this.app.use(express.json());
+    this.app.use(cors());
+  }
+
+  #initHomeRoute() {
+    this.app.get("/", () => {
+      res.send("You have arrived at the sasta VTOP api");
+    });
+  }
+
+  #listen() {
+    this.app.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}`);
+    });
+  }
+}
